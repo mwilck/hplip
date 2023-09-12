adfabd01bd9257e4c33fa4a98f5b744a8e656700
Martin Wilck <mwilck@suse.com>
2023-09-12 11:05:25 +0200

    unpack-hplip.sh: try to set commit date correctly

--- git-helpers/unpack-hplip.sh
+++ git-helpers/unpack-hplip.sh
@@ -16,14 +16,15 @@ unpack_hplip() {
     tar xz -C "$TD" -f "/tmp/hplip-$ver.tar.gz"
     rsync -ai --delete --exclude=.git\* --exclude=git-helpers \
 	  "$TD/hplip-$ver/" "$TARGET" 2>&1 | tee "$TD/rsync.txt"
+    newest="$(sed -n '/^>f/s/^.\{12\}//p' "$TD/rsync.txt" | xargs ls -t 2>/dev/null | head -1)"
+    datum=$(stat -c %Y "$newest")
+    echo "newest file: $newest $(stat -c %y $newest)" >&2
     find . -name '*.gz' | xargs gzip -dvf 2>&1 | tee "$TD/uncompressed.txt"
     sed -i 's/\.gz:.*//' "$TD/uncompressed.txt"
     cat git-helpers/uncompressed.txt "$TD/uncompressed.txt" | sort -u >"$TD/new.txt"
     cp "$TD/new.txt" git-helpers/uncompressed.txt
     git add .
-    echo "entering shell"
-    bash
-    git commit -m "hplip $ver"
+    git commit --date "$datum +0000" -m "hplip $ver"
     git tag "$ver"
 }
 
